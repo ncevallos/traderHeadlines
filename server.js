5fc10c331cd7a646c4c90d86719e955952f24f8d
ncevallos <nmcevall@gmail.com>
2017-10-26 20:16:46 -0400

    changed mongoose connect

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ app.use(express.static("public"));
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/articles", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
